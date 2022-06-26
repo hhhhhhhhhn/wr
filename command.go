a8f1b3bfc609d29f8cc58de0aa045dea18aa4e9d
hhhhhhhhhn <joseugarte2262005@gmail.com>
2022-06-25 20:51:53 -0400

    Add line number to status bar and clean up render
    Refactoring efforts will have to begin soon

--- command.go
+++ command.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"strings"
+
 	"github.com/hhhhhhhhhn/hexes"
+	"github.com/hhhhhhhhhn/wr/core"
 )
 
 var modes = []string{}
@@ -22,13 +25,23 @@ func updateModeString() {
 	modeString = strings.Join(modes, " > ")
 }
 
-func printStatusBar(r *hexes.Renderer, modeString string) {
+func printStatusBar(e *core.Editor, r *hexes.Renderer, modeString string) {
 	row := r.Rows - 1
+	var position string
+	if len(e.Cursors) > 0 {
+		position = fmt.Sprintf("line %v, col %v ",
+			editor.Cursors[len(editor.Cursors)-1].Start.Row,
+			editor.Cursors[len(editor.Cursors)-1].Start.Column,
+		)
+	}
+
+	modeString = " " + modeString
 	if len(modeString) < r.Cols {
 		modeString += strings.Repeat(" ", r.Cols - len(modeString))
 	}
 	r.SetAttribute(hexes.REVERSE)
 	r.SetString(row, 0, modeString)
+	r.SetString(row, r.Cols-len(position), position)
 }
 
 func commandMode() {
--- render.go
+++ render.go
@@ -7,55 +7,70 @@ import (
 	"github.com/hhhhhhhhhn/wr/core"
 )
 
-func PrintEditor(e *core.Editor, r *hexes.Renderer) {
-	renderRows := r.Rows - 1 // Extra row for commands
-
+func handleScroll(e*core.Editor, renderRows int, currentScroll int) (newScroll int) {
 	var lastCursorRow int
 	if len(editor.Cursors) > 0 {
 		lastCursorRow = e.Cursors[len(e.Cursors) - 1].Start.Row
 	} else {
 		lastCursorRow = 0
 	}
-	if lastCursorRow < scroll {
-		scroll = lastCursorRow
+	if lastCursorRow < currentScroll {
+		currentScroll = lastCursorRow
 	}
-	if lastCursorRow > scroll + renderRows - 1 {
-		scroll = lastCursorRow - renderRows + 1
+	if lastCursorRow > currentScroll + renderRows - 1 {
+		currentScroll = lastCursorRow - renderRows + 1
 	}
+	return currentScroll
+}
+
+func padWithSpaces(str string, cols int, desiredCols int) string {
+	if cols < desiredCols {
+		str += strings.Repeat(" ", desiredCols - cols)
+	}
+	return str
+}
+
+func getLineAsString(e *core.Editor, row int) string {
+	return strings.ReplaceAll(string(e.Buffer.GetLine(row)), "\t", strings.Repeat(" ", e.Config.Tabsize))
+}
+
+func printLine(e *core.Editor, r *hexes.Renderer, row int) {
+	line := getLineAsString(e, row)
+	line = padWithSpaces(line, core.ColumnSpan(e, []rune(line)), r.Cols)
+
+	col := 0
+	for _, chr := range line {
+		withinCursor, withinLast := isWithinCursor(e, row, col)
+		if withinCursor {
+			if withinLast {
+				r.SetAttribute(hexes.MAGENTA + hexes.REVERSE)
+			} else {
+				r.SetAttribute(hexes.REVERSE)
+			}
+		} else {
+			r.SetAttribute(r.DefaultAttribute)
+		}
+		r.SetString(row - scroll, col, string(chr))
+		col += core.RuneWidth(e, chr)
+	}
+}
+
+func PrintEditor(e *core.Editor, r *hexes.Renderer) {
+	renderRows := r.Rows - 1 // Extra row for commands
+	scroll = handleScroll(e, renderRows, scroll)
 
 	lineAmount := e.Buffer.GetLength()
 
 	var row int
 	for row = scroll; row < scroll + renderRows && row < lineAmount; row++ {
-		line := strings.ReplaceAll(string(e.Buffer.GetLine(row)), "\t", strings.Repeat(" ", e.Config.Tabsize))
-		columnSpan := core.ColumnSpan(e, []rune(line))
-
-		if columnSpan < r.Cols {
-			line += strings.Repeat(" ", r.Cols - columnSpan)
-		}
-
-		col := 0
-		for _, chr := range line {
-			withinCursor, withinLast := isWithinCursor(e, row, col)
-			if withinCursor {
-				if withinLast {
-					r.SetAttribute(hexes.MAGENTA + hexes.REVERSE)
-				} else {
-					r.SetAttribute(hexes.REVERSE)
-				}
-			} else {
-				r.SetAttribute(r.DefaultAttribute)
-			}
-			r.SetString(row - scroll, col, string(chr))
-			col += core.RuneWidth(e, chr)
-		}
+		printLine(e, r, row)
 	}
 
 	for ;row < scroll + renderRows; row++ {
 		r.SetString(row - scroll, 0, strings.Repeat(" ", r.Cols))
 	}
 
-	printStatusBar(r, modeString)
+	printStatusBar(e, r, modeString)
 
 	out.Flush()
 }
